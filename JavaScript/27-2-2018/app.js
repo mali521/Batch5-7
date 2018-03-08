f7ce824faa64b0ab2e1abcc9476fb844d0debbb2
Muhammad Ali Mughal <mamughal007@gmail.com>
2018-03-08 12:01:26 +0500

    Class Code

---
+++ JavaScript/27-2-2018/app.js
@@ -0,0 +1,80 @@
+// function tellTime() {
+//     var now = new Date();
+//     var theHr = now.getHours();
+//     var theMin = now.getMinutes();
+//     alert("Current time: " + theHr + ":" + theMin);
+// }
+
+// tellTime();
+
+
+// function add(){
+//     var a = +prompt("Enter value");
+//     var b = +prompt("Enter value");
+//     alert(a+b)
+// }
+
+// add();
+
+
+// function add(a,b,c){
+
+//     alert(a+b+c);
+// }
+
+// add(2,3)
+// var c = "hello world";
+
+// function ab(a){
+//     alert(a);
+// }
+
+// ab(c);
+
+// var month = "MArch";
+// var text = "is a cool month";
+// var born = "i also born in march";
+// function aa(a,b,c){
+//     alert(a +"\n"+ b + "\n"+ c);
+// }
+
+// aa(month,text,born);
+
+// function calcTot(merchTot) {
+//     var orderTot;
+//     if (merchTot >= 100) {
+//         orderTot = merchTot;
+//     }
+//     else if (merchTot < 50.01) {
+//         orderTot = merchTot + 5;
+//     }
+//     else {
+//         orderTot = merchTot + 5 + (.03 * (merchTot - 50));
+//     }
+//     return orderTot;
+// }
+// var a = calcTot(70);
+
+// alert(a)
+
+
+
+// var a = 12;
+// function abc(){
+//     alert(a);
+// }
+// abc();
+
+var b = 7;
+
+function xyz(){
+    b = 5;
+    alert(b);
+}
+
+// alert(b);
+
+xyz();
+
+
+
